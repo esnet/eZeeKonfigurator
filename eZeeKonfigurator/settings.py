2af0c75eabd5ee08b06e7fdd5a0ad0fc72d4c187
Vlad Grigorescu <grigorescu@gmail.com>
2019-08-14 15:42:22 -0500

    Enable debug logging.

--- eZeeKonfigurator/settings.py
+++ eZeeKonfigurator/settings.py
@@ -120,3 +120,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
