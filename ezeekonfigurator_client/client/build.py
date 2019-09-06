8a0f1c590276431318ecd1269c36ecce3d6068ac
Vlad Grigorescu <grigorescu@gmail.com>
2019-09-06 00:13:15 -0500

    Move templates

--- ezeekonfigurator_client/client/build.py
+++ ezeekonfigurator_client/client/build.py
@@ -1,5 +1,14 @@
+import os
 import sys
 import uuid
 
+url = sys.argv[1]
+install_dir = sys.argv[2]
+conf = os.path.join(install_dir, "ezeekonfigurator_client", "conf.dat")
+
 with open("scripts/conf.dat", 'w') as f:
-    f.write("eZeeKonfigurator::server_endpoint\t%s/client_api/v1/%s/\n" % (sys.argv[1], uuid.uuid4()))
+    if os.path.exists(conf) and os.path.isfile(conf):
+        f.write(open(conf).read())
+    else:
+        f.write("eZeeKonfigurator::server_endpoint\t%s/client_api/v1/%s/\n" % (url, uuid.uuid4()))
+
