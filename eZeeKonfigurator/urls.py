8600d29ce5ef0f5a849a5fad1d4f5e5cf4ff24b9
Vlad Grigorescu <grigorescu@gmail.com>
2019-09-29 22:41:45 -0500

    Add links to the page to edit an option.

--- eZeeKonfigurator/urls.py
+++ eZeeKonfigurator/urls.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('', webui.home, name='home'),
     path('sensors/auth', webui.list_sensors, name='list_sensors'),
     path('sensors/settings', webui.list_options, name='list_options'),
+    path('sensors/edit_option/<int:id>', webui.edit_option, name='edit_option'),
+
     #path('client_api/v<int:ver>/<slug:sensor_uuid>/option_list/', webconfig.views.client_api_option_list, name='option_list'),
     path('brokerd_api/<slug:brokerd_uuid>/v<int:ver>/sensor_info/', brokerd_api.sensor_info, name='sensor_info'),
     path('brokerd_api/<slug:brokerd_uuid>/v<int:ver>/sensor_option/', brokerd_api.sensor_option, name='sensor_option'),
