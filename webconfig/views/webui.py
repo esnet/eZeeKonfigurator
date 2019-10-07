3b826cb011c6cb3c2409886f0ea6df3ddb2be076
Vlad Grigorescu <grigorescu@gmail.com>
2019-10-06 23:38:28 -0500

    Fix change event type.

--- webconfig/views/webui.py
+++ webconfig/views/webui.py
@@ -359,7 +359,7 @@ def edit_option(request, id):
             form.save()
             new = str(s.value)
             name = s.option.get_name()
-            change_event = {'type': "change", 'option': name, 'val': s.value.json(), 'zeek_type': data['type'], 'uuid': s.option.sensor.uuid}
+            change_event = {'type': "change", 'option': name, 'val': s.value.json(), 'zeek_type': models.get_name_of_model(s.value), 'uuid': s.option.sensor.uuid}
             send_event('test', 'message', change_event)
             data['success'] = ["Changes saved: %s -> %s" % (old, new)]
         data['form'] = forms.get_form_for_model(s.value)
