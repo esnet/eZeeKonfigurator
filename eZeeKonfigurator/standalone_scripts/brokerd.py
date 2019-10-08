e29c5e5684b225471828fd485cd3a10a6d0e0aa6
Vlad Grigorescu <grigorescu@gmail.com>
2019-10-08 09:42:59 -0500

    Support blocking.

--- eZeeKonfigurator/standalone_scripts/brokerd.py
+++ eZeeKonfigurator/standalone_scripts/brokerd.py
@@ -1,16 +1,40 @@
 import aiohttp
 from aiohttp_sse_client import client as sse_client
 import asyncio
-import broker
 import datetime
 import hashlib
 import json
 import logging
 import os
 import requests
+import sys
 
 from eZeeKonfigurator.utils import from_json, to_json
 
+# Import broker. Just do it?
+try:
+    import broker
+except ImportError:
+    # Next, we'll try to use the broker from zeekctl, but we need to find it.
+    # We'll find it via zeek-config --python_dir
+
+    broker_error_message = "Could not import the Python Broker bindings. See: https://docs.zeek.org/projects/broker/en/stable/python.html#installation-in-a-virtual-environment"
+
+    import distutils.spawn
+
+    which_zeekconfig = distutils.spawn.find_executable('zeek-config')
+    if not which_zeekconfig:
+        raise ImportError(broker_error_message)
+
+    python_dir = os.popen('zeek-config --python_dir').read().strip()
+    sys.path.append(python_dir)
+    sys.path.append(os.path.join(python_dir, "broker"))
+
+    try:
+        import broker
+    except ImportError:
+            # We've done all we can
+            raise ImportError(broker_error_message)
 
 debug = True
 
